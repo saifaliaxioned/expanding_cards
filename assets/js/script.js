59dfad7f8bd1b2ef91af25c573d83907b88e2b0c
saif <saifalis@axioned.com>
2022-09-20 15:48:54 +0530

    js code added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
-/* Author: 
+var gallery = document.querySelector('.gallery');
+var imageList = document.querySelectorAll('.image-list');
 
-*/
+imageList.forEach(function expand(a) {
+  a.addEventListener('click',function listner() {
+    var activeImage = document.querySelector('.active-image');
+    activeImage.classList.remove('active-image');
+    a.classList.add('active-image');
+    a.children[0].children[1].style.display = "block";
+  });
+})
 
 
 

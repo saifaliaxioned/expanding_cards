ad884602f562d138531215f250bc4ee0b763aa31
saif <saifalis@axioned.com>
2022-09-20 16:07:06 +0530

    js code completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 var gallery = document.querySelector('.gallery');
 var imageList = document.querySelectorAll('.image-list');
-
-imageList.forEach(function expand(a) {
+var title = document.querySelectorAll('.title');
+imageList.forEach(function expand(a, i) {
   a.addEventListener('click',function listner() {
     var activeImage = document.querySelector('.active-image');
+    var activeTitle = document.querySelector('.active-title');
+    activeTitle.classList.remove('active-title');
     activeImage.classList.remove('active-image');
     a.classList.add('active-image');
-    a.children[0].children[1].style.display = "block";
+    title[i].classList.add('active-title');
   });
 })
 

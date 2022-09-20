ce6c826d9d59cfb70e9b4b5e8822e33d2b9e1e1d
saif <saifalis@axioned.com>
2022-09-20 19:39:01 +0530

    parameter name added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
+// Image expand function
 var gallery = document.querySelector('.gallery');
 var imageList = document.querySelectorAll('.image-list');
 var title = document.querySelectorAll('.title');
-imageList.forEach(function expand(a, i) {
-  a.addEventListener('click',function listner() {
+imageList.forEach(function expand(li, titleIndex) {
+  li.addEventListener('click',function listner() {
     var activeImage = document.querySelector('.active-image');
     var activeTitle = document.querySelector('.active-title');
     activeTitle.classList.remove('active-title');
     activeImage.classList.remove('active-image');
-    a.classList.add('active-image');
-    title[i].classList.add('active-title');
+    li.classList.add('active-image');
+    title[titleIndex].classList.add('active-title');
   });
 })
 
